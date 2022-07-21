a9217bcab006aa5322b2c88e2e56940fa7d55a31
sophia.armond <sophis.armond14@gmail.com>
2022-07-20 21:45:24 -0300

    Meu projeto senac store

--- Store/admin.py
+++ Store/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
+
+from Store.models import Categoria, Departamento, Produto
 
 # Register your models here.
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome']
+    list_display_links = ['id', 'nome']
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'departamento']
+    list_display_links = ['id', 'nome']
+    list_filter = ['departamento']
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'preco', 'estoque', 'categoria', 'ver_imagem']
+    list_display_links = ['id', 'nome']
+    list_filter = ['categoria']
+    serach_fields = ['nome']
+    readonly_fields = ['ver_imagem']
+
+    def ver_imagem(self,obj):
+        return mark_safe('<img src="{url}" widht="{widht}"height="{height}" />'.format(
+            url = obj.imagem.url,
+            width=75,
+            height=75,
+        )
+    )
+    
+admin.site.register(Departamento)
+admin.site.register(Categoria)
+admin.site.register(Produto)
--- Store/models.py
+++ Store/models.py
@@ -1,3 +1,29 @@
+from distutils.command.upload import upload
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Departamento(models.Model):
+    nome = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=8000)
+    imagem = models.ImageField(upload_to='images/')
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    estoque = models.IntegerField()
+    lancamento = models.BooleanField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
