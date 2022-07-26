3bc36959f6943e26da0cb90d248ef4d2db190141
sophia.armond <sophis.armond14@gmail.com>
2022-07-25 22:18:32 -0300

    atlz

--- Store/views.py
+++ Store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from Store.models import Departamento
 
 # Create your views here.
+def index(request):
+    meu_nome = 'Sophia Armond Santos'
+    sexo = 'F'
+    context = {'nome': meu_nome,'artigo': 'o' if sexo == 'M' else 'a'}
+    return render(request, 'index.html', context)
+
+def teste(request):
+    # depto = ['TI', 'Casa', 'Telefonia', 'Logística', 'Informática']
+    depto = Departamento.objects.all()
+    context = {'departamentos': depto}
+    return render(request, 'teste.html', context)
